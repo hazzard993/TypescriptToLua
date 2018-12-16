552e014712a5eda6f45fe10b533f14c2b224fb25
hazzard993 <hazzard993@gmail.com>
2018-12-17 08:18:41 +1000

    Updated return translation tests

--- test/translation/gml/return.gml.gml
+++ test/translation/gml/return.gml.gml
@@ -5,10 +5,34 @@ _[1] = 2;
 _[2] = 3;
 return _;
 return 3+5;
-var x = 5;
 var _ = ds_map_create();
 _[?1+2] = 5;
 _[?"x"] = x;
 _[?"y"] = 5;
 _[?"z"] = 6;
 return _;
+if true
+{
+    return;
+}
+if true
+{
+    var _;
+    _[0] = 1;
+    _[1] = 2;
+    _[2] = 3;
+    return _;
+}
+if true
+{
+    return 3+5;
+}
+if true
+{
+    var _ = ds_map_create();
+    _[?1+2] = 5;
+    _[?"x"] = x;
+    _[?"y"] = 5;
+    _[?"z"] = 6;
+    return _;
+}
--- test/translation/ts/return.gml.ts
+++ test/translation/ts/return.gml.ts
@@ -1,10 +1,46 @@
+// Just a return statement
+// @ts-ignore
 return;
+
+// Return array
+// @ts-ignore
 return [1, 2, 3];
+
+// Return expression result
+// @ts-ignore
 return 3 + 5;
-let x = 5;
+
+// Return json/ds_map
+// @ts-ignore
 return {
     [1 + 2]: 5,
     x,
     "y": 5,
     z: 6,
 };
+
+// Indent tests
+if (true) {
+    // @ts-ignore
+    return;
+}
+
+if (true) {
+    // @ts-ignore
+    return [1, 2, 3];
+}
+
+if (true) {
+    // @ts-ignore
+    return 3 + 5;
+}
+
+if (true) {
+    // @ts-ignore
+    return {
+        [1 + 2]: 5,
+        x,
+        "y": 5,
+        z: 6,
+    };
+}
